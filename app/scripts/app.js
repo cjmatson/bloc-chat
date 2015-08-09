39fcb53ea075261c200b5dbfef413dfa9c5472d2
cjmatson <matson.cj@gmail.com>
2015-08-09 14:49:08 -0500

    Create Chat Rooms

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
-var blocChat = angular.module('BlocChat', ['ui.router', 'firebase']);
+var blocChat = angular.module('BlocChat', ['ui.router', 'firebase', 'ui.bootstrap']);
 blocChat.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
-	$locationProvider.html5Mode(true);
+	$locationProvider.html5Mode({ enabled: true, requireBase: false });
 	$stateProvider.state('home', {
 		url: '/',
 		controller: 'Home.controller',
@@ -8,17 +8,49 @@ blocChat.config(['$stateProvider', '$locationProvider', function($stateProvider,
 	});
 }])
 
-blocChat.controller('Home.controller', ['$scope', 'Room', function($scope, Room) {
-	$scope.chatRoom = {
-		room: [{name: "Chat Room 1"}, {name: "Chat Room 2"}, {name: "Chat Room 3"}]
-	};
+blocChat.controller('Home.controller', ['$scope', 'Room', '$rootScope', '$modal', function($scope, Room, $rootScope, $modal) {
+	$scope.animationsEnabled = true;
+	$scope.toggleAnimations = function() {
+		$scope.animationsEnabled = !$scope.animationsEnabled;
+	}
+	$scope.open = function(size) {
+		var modalInstance = $modal.open({
+		animation: $scope.animationsEnabled,
+		size: size,
+		scope: $scope,
+		replace: true,
+		controller: 'ModalContent.controller',
+		templateUrl: '/templates/modalcontent.html',
+		windowClass: 'modal fade in'
+		});
+	}
 }])
 
-blocChat.factory('Room', ['$firebase', function($firebase) {
-	var ref = new Firebase("https://screaming-wind-7497.firebaseio.com/");
-	var rooms = $firebase(ref.child('rooms')).$asArray();
+blocChat.controller('ModalContent.controller', ['$scope', 'Room', '$rootScope', '$modalInstance', function($scope, Room, $rootScope, $modalInstance) {
+	$scope.cancel = function() {
+		$modalInstance.dismiss('cancel');
+	}
+	$scope.createRoom = function() {
+		if ($rootScope.room) {
+			$rootScope.rooms.$add($rootScope.room);
+			$modalInstance.close();
+		}
+	}
+	$scope.returnRoom = function($event) {
+		if($rootScope.room && $event.keyCode === 13) {
+			$rootScope.rooms.$add($rootScope.room);
+			$modalInstance.close();
+		}
+	}
+}])
+
+blocChat.factory('Room', ['$firebase', '$rootScope', function($firebase, $rootScope) {
+	var ref = new Firebase ("https://screaming-wind-7497.firebaseio.com/");
+	$rootScope.data = $firebase(ref);
+	$rootScope.rooms = $rootScope.data.$asArray();
+	$rootScope.room = {name: $rootScope.room};
 	return {
-		all: rooms
+		all: $rootScope.rooms
 	}
 }])
 
