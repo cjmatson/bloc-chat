60ca4511b1484382187552989bac29b2aae4bff2
cjmatson <matson.cj@gmail.com>
2015-08-25 13:55:20 -0500

    Set a Username

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,4 @@
-var blocChat = angular.module('BlocChat', ['ui.router', 'firebase', 'ui.bootstrap']);
+var blocChat = angular.module('BlocChat', ['ui.router', 'firebase', 'ui.bootstrap', 'ngCookies']);
 blocChat.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
 	$locationProvider.html5Mode({ enabled: true, requireBase: false });
 	$stateProvider.state('home', {
@@ -7,32 +7,59 @@ blocChat.config(['$stateProvider', '$locationProvider', function($stateProvider,
 		templateUrl: '/templates/home.html'
 	});
 }])
-blocChat.controller('Home.controller', ['$scope', 'Room', '$modal', function($scope, Room, $modal) {
+
+blocChat.run(['$modal', '$cookies', function ($modal, $cookies) {
+	if (!$cookies.currentUser || $cookies.currentUser === "") {
+		$modal.open({
+			replace: true,
+			controller: 'ModalCurrentUser.controller',
+			templateUrl: '/templates/modalcurrentuser.html',
+			windowClass: 'modal fade in',
+			backdrop: 'static'
+		})
+	}
+}])
+
+blocChat.controller('ModalCurrentUser.controller', ['$scope', '$modalInstance', '$cookies', function ($scope, $modalInstance, $cookies) {
+	$scope.currentUser = {};
+	$scope.enterUsernameButton = function () {
+		if (!$scope.currentUser.name) {
+			alert("Invalid username. Please enter a valid username.");
+		}
+		else {
+			$cookies.put("currentUser", $scope.currentUser.name);
+			$modalInstance.close();
+		}
+	}
+}])
+
+blocChat.controller('Home.controller', ['$scope', 'Room', '$modal', function ($scope, Room, $modal) {
 	$scope.rooms = Room.all;
-	$scope.open = function(size) {
+	$scope.open = function (size) {
 		var modalInstance = $modal.open({
 		size: size,
 		scope: $scope,
 		replace: true,
 		controller: 'ModalContent.controller',
 		templateUrl: '/templates/modalcontent.html',
-		windowClass: 'modal fade in'
+		windowClass: 'modal fade in',
+		backdrop: 'static'
 		});
 	}
 	$scope.activeChatRoom = false;
-	$scope.selectChatRoom = function(room, id) {
+	$scope.selectChatRoom = function (room, id) {
 		$scope.selectedChatRoom = room.name;
 		$scope.activeChatRoom = true;
 		$scope.messages = Room.messages(id);
 	}
 }])
 
-blocChat.controller('ModalContent.controller', ['$scope', 'Room', '$modalInstance', function($scope, Room, $modalInstance) {
+blocChat.controller('ModalContent.controller', ['$scope', 'Room', '$modalInstance', function ($scope, Room, $modalInstance) {
 	$scope.room = {};
-	$scope.cancel = function() {
+	$scope.cancel = function () {
 		$modalInstance.dismiss('cancel');
 	}
-	$scope.createRoom = function() {
+	$scope.createRoom = function () {
 		if ($scope.room.name) {
 		Room.create($scope.room);
 		$modalInstance.close();
@@ -40,15 +67,15 @@ blocChat.controller('ModalContent.controller', ['$scope', 'Room', '$modalInstanc
 	}
 }])
 
-blocChat.factory('Room', ['$firebaseArray', function($firebaseArray) {
+blocChat.factory('Room', ['$firebaseArray', function ($firebaseArray) {
 	var ref = new Firebase ("https://screaming-wind-7497.firebaseio.com/");
 	var rooms = $firebaseArray(ref.child('rooms'));
 	return {
 		all: rooms,
-		create: function(newRoom) {
+		create: function (newRoom) {
 			rooms.$add(newRoom);
 		},
-		messages: function(roomId) {
+		messages: function (roomId) {
 			var messagesRef = new Firebase ("https://screaming-wind-7497.firebaseio.com/messages")
 			return $firebaseArray(messagesRef.orderByChild('roomId').equalTo(roomId));
 		}
